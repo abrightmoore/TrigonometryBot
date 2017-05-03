39ed3d9565c26d0ec64316e5212addf55b6d1a3e
Adrian Brightmoore <abrightmoore@yahoo.com.au>
2017-05-04 00:12:10 +1000

    calcLinesSmooth in ImageTools

--- ImageTools.py
+++ ImageTools.py
@@ -8,6 +8,71 @@ import sys
 
 from PIL import Image, ImageDraw
 
+def flatten(anArray):
+	result = []
+	for a in anArray:
+		for b in a:
+			result.append(b)
+	return result
+
+def calcLinesSmooth(SMOOTHAMOUNT,P):
+	Q = []
+	for i in xrange(0,SMOOTHAMOUNT):
+		P = chaikinSmoothAlgorithm(P)
+	Q = calcLines(P)
+	return flatten(Q)
+
+def calcLines(P):
+	Q = []
+	count = 0
+	(x0,y0,z0) = (0,0,0)
+	for (x,y,z) in P:
+		if count > 0:
+			Q.append( calcLine((x0,y0,z0),(x,y,z)) )
+		count = count+1
+		(x0,y0,z0) = (x,y,z)
+	return Q
+
+def calcLine((x,y,z), (x1,y1,z1) ):
+	return calcLineConstrained((x,y,z), (x1,y1,z1), 0 )
+
+def calcLineConstrained((x,y,z), (x1,y1,z1), maxLength ):
+	dx = x1 - x
+	dy = y1 - y
+	dz = z1 - z
+
+	distHoriz = dx*dx + dz*dz
+	distance = sqrt(dy*dy + distHoriz)
+	P = []
+	if distance < maxLength or maxLength < 1:
+		phi = atan2(dy, sqrt(distHoriz))
+		theta = atan2(dz, dx)
+
+		iter = 0
+		while iter <= distance:
+			(xd,yd,zd) = ((int)(x+iter*cos(theta)*cos(phi)), (int)(y+iter*sin(phi)), (int)(z+iter*sin(theta)*cos(phi)))
+			# setBlock(scratchpad,(blockID,blockData),xd,yd,zd)
+			P.append((xd,yd,zd))
+			iter = iter+0.5 # slightly oversample because I lack faith.
+	return P # The set of all the points calc'd
+	
+def chaikinSmoothAlgorithm(P): # http://www.idav.ucdavis.edu/education/CAGDNotes/Chaikins-Algorithm/Chaikins-Algorithm.html
+	F1 = 0.25
+	F2 = 0.75
+	Q = []
+	(x0,y0,z0) = (-1,-1,-1)
+	count = 0
+	for (x1,y1,z1) in P:
+		if count > 0: # We have a previous point
+			(dx,dy,dz) = (x1-x0,y1-y0,z1-z0)
+			Q.append( (x0*F2+x1*F1,y0*F2+y1*F1,z0*F2+z1*F1) )
+			Q.append( (x0*F1+x1*F2,y0*F1+y1*F2,z0*F1+z1*F2) )
+		else:
+			count = count+1
+		(x0,y0,z0) = (x1,y1,z1)
+
+	return Q
+
 def getFormulas():
 	formulaR = makeRandomFormula()
 	formulaG = makeRandomFormula()
